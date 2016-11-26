a3a55c64118da25558d5d5b4dcd4a7eb285e34fc
Javier Forero <javierforero01@gmail.com>
2016-11-26 12:48:10 -0600

    deleted users controller and created new controller that inherits from registered controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'registrations'
+  }
   resources :users, only: [:index, :show, :create, :update, :destroy]
+
 end
