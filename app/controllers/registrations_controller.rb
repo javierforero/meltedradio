1c66117cdf0adcdfd620add8948d843e93a2aacf
Javier Forero <javierforero01@gmail.com>
2016-11-26 12:55:58 -0600

    fixed unpermitted attributes. flash error message

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.permit(:name, :email, :password, :password_confirmation)
+    params.require(:registration).permit(:name, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.permit(:name, :email, :password, :password_confirmation)
+    params.require(:registration).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
