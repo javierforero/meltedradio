034d856efb625ae4f9bda4f2ee4448c9dcc2fa17
Javier Forero <javierforero01@gmail.com>
2016-12-08 12:46:35 -0600

    grunt running still not talking to rails yet

---
+++ client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meltedRadioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the meltedRadioApp
+ */
+angular.module('meltedRadioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
