c1bc6eb2cfc9892f29c272d8435b4acef31223ab
Javier Forero <javierforero01@gmail.com>
2016-12-13 13:41:56 -0600

    still working on getting users view front end

--- client/src/app/index.route.js
+++ client/src/app/index.route.js
@@ -6,6 +6,11 @@ export function routerConfig ($stateProvider, $urlRouterProvider) {
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
       controllerAs: 'main'
+    })
+    .state('users', {
+      url: '/users',
+      templateUrl: 'app/templates/users.html',
+      controller: 'UsersCtrl as users'
     });
 
   $urlRouterProvider.otherwise('/');
