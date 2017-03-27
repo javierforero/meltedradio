12f244593f19c4b55058c39a6df646df305a7988
Javier Forero <javierforero01@gmail.com>
2017-03-27 08:45:32 -0500

    started inplementing youtube iframe api

--- client/src/app/scripts/controllers/HomeCtrl.js
+++ client/src/app/scripts/controllers/HomeCtrl.js
@@ -178,5 +178,49 @@ angular.module('meltedRadio')
          $('ul#'+videoId).toggle("slow");
        };
 
+       $scope.play = function() {
+
+         var tag = document.createElement('script');
+         tag.src = "https://www.youtube.com/iframe_api";
+         var videoTag = document.getElementsByClassName('playlist-video-info')[0];
+         videoTag.parentNode.insertBefore(tag, videoTag);
+
+         var player;
+         function onYouTubeIframeAPIReady() {
+           player = new YT.Player('iframe-player', {
+             height: '390',
+             width: '640',
+             videoId: 'M7lc1UVf-VE',
+             events: {
+               'onReady': onPlayerReady,
+               'onStateChange': onPlayerStateChange
+             }
+           });
+         }
+
+         console.log(player);
+
+         // 4. The API will call this function when the video player is ready.
+         function onPlayerReady(event) {
+           event.target.playVideo();
+         }
+
+         // 5. The API calls this function when the player's state changes.
+         //    The function indicates that when playing a video (state=1),
+         //    the player should play for six seconds and then stop.
+         var done = false;
+         function onPlayerStateChange(event) {
+           if (event.data == YT.PlayerState.PLAYING && !done) {
+             setTimeout(stopVideo, 6000);
+             done = true;
+           }
+         }
+         function stopVideo() {
+           player.stopVideo();
+         }
+
+
+       };
+
 
     }]);
