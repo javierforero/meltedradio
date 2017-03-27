97aa74e3f9d581a696ca84e7c9d78e246f95cf0d
Javier Forero <javierforero01@gmail.com>
2017-03-27 15:31:16 -0500

    got player working found add new playlist issue

--- client/src/app/scripts/controllers/HomeCtrl.js
+++ client/src/app/scripts/controllers/HomeCtrl.js
@@ -27,6 +27,8 @@ angular.module('meltedRadio')
        $scope.userSignedIn = localStorageService.get('currentUser');
        $scope.currentPlaylist = null;
        $scope.songs = null;
+       $scope.currentSong = null;
+       var player;
 
        (function changeNavColor(){
          $('nav.nav-bar').css('color','white');
@@ -178,46 +180,32 @@ angular.module('meltedRadio')
          $('ul#'+videoId).toggle("slow");
        };
 
+       $scope.onPlayerReady = function(event) {
+         event.target.playVideo();
+       };
+
+       $scope.onPlayerStateChange = function(event) {
+
+       };
+
+       function stopVideo() {
+
+         player.stopVideo();
+       }
+
        $scope.play = function() {
+           var vidHeight = $('div.video').height();
+           var vidWidth = $('div.video').width();
 
-         var tag = document.createElement('script');
-         tag.src = "https://www.youtube.com/iframe_api";
-         var videoTag = document.getElementsByClassName('playlist-video-info')[0];
-         videoTag.parentNode.insertBefore(tag, videoTag);
-
-         var player;
-         function onYouTubeIframeAPIReady() {
-           player = new YT.Player('iframe-player', {
-             height: '390',
-             width: '640',
-             videoId: 'M7lc1UVf-VE',
+           player = new YT.Player('iframe-utube-player', {
+             height: vidHeight,
+             width:  vidWidth,
+             videoId: $scope.songs()[0].url,
              events: {
-               'onReady': onPlayerReady,
-               'onStateChange': onPlayerStateChange
+               'onReady': $scope.onPlayerReady,
+               'onStateChange': $scope.onPlayerStateChange
              }
            });
-         }
-
-         console.log(player);
-
-         // 4. The API will call this function when the video player is ready.
-         function onPlayerReady(event) {
-           event.target.playVideo();
-         }
-
-         // 5. The API calls this function when the player's state changes.
-         //    The function indicates that when playing a video (state=1),
-         //    the player should play for six seconds and then stop.
-         var done = false;
-         function onPlayerStateChange(event) {
-           if (event.data == YT.PlayerState.PLAYING && !done) {
-             setTimeout(stopVideo, 6000);
-             done = true;
-           }
-         }
-         function stopVideo() {
-           player.stopVideo();
-         }
 
 
        };
