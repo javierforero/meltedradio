f2edb258187c4f4e00fb124a973139b5d596d903
Javier Forero <javierforero01@gmail.com>
2017-06-01 08:36:07 -0500

    almost there

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,10 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # static assets
+
+  config.serve_static_assets = true
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
